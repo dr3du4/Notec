7645fa8b39f6ebc018d4c97a167a8581a729fd8a
Jakub Szymczak <szymczak.kubus@gmail.com>
2024-09-28 17:35:28 +0200

    Add custom colors to tailwind

--- notec-frontend/tailwind.config.js
+++ notec-frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple-dark": "#72369D",
+        "custom-purple-light": "#B67BE6",
+        "custom-yellow-dark": "#FA8C02",
+        "custom-yellow-light": "#FFC242",
+      },
+    },
   },
   plugins: [],
 };
