693216a1bf91da60bc732a35a7bda7499c3f5ceb
Jakub Szymczak <szymczak.kubus@gmail.com>
2024-09-28 20:08:40 +0200

    Add Route to Profile Page

--- notec-frontend/src/App.tsx
+++ notec-frontend/src/App.tsx
@@ -6,12 +6,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/user/:id" element={<ProfilePage />} />
       </Route>,
     ),
   );
