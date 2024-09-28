0ba4fd2145257b23af467c64696203ed5cb6e89b
Piotr Zapiór <piotrzapior0@gmail.com>
2024-09-28 22:43:15 +0200

    zaznaczanie odp

--- notec-frontend/src/components/QuestionCard.tsx
+++ notec-frontend/src/components/QuestionCard.tsx
@@ -4,6 +4,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import {useState} from "react";
 
 interface QuestionCardProps {
     title: string;
@@ -11,6 +12,12 @@ interface QuestionCardProps {
 }
 
 export default function QuestionCard({ title, options }: QuestionCardProps) {
+    const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+    const handleOptionClick = (option: string) => {
+        setSelectedOption(option);
+    };
+
     return (
         <Card sx={{ maxWidth: '80vw', width: '80vw', minHeight: '8rem', maxHeight: '15rem', marginBottom: '1rem' }}>
             <CardContent>
@@ -18,9 +25,23 @@ export default function QuestionCard({ title, options }: QuestionCardProps) {
                     {title}
                 </Typography>
             </CardContent>
-            <CardActions sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%' }}>
+            <CardActions sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '100%'}}>
                 {options.map((option, index) => (
-                    <Button key={index} size="small" sx={{ justifyContent: 'flex-start', width: '100%', marginLeft: 1}}>
+                    <Button
+                        key={index}
+                        size="small"
+                        onClick={() => handleOptionClick(option.option)}
+                        sx={{
+                            justifyContent: 'flex-start',
+                            width: '100%',
+                            marginLeft: 1,
+                            textAlign: 'left',
+                            minHeight: '36px',
+                            color: 'black',
+                            backgroundColor: selectedOption === option.option ? '#FFC242' : 'transparent',
+
+                        }}
+                    >
                         {option.option}
                     </Button>
                 ))}
