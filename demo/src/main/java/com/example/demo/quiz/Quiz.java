11e0babba6d9f2c14065802821c33c7ab46a8c9b
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2024-09-28 18:28:56 +0200

    Add Quiz Model

---
+++ demo/src/main/java/com/example/demo/quiz/Quiz.java
@@ -0,0 +1,48 @@
+package com.example.demo.quiz;
+
+import javax.persistence.*;
+import javax.persistence.GenerationType;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.util.List;
+
+@Entity
+@Table
+public class Quiz {
+
+    @Id
+    @SequenceGenerator(
+            name = "quiz_sequence",
+            sequenceName = "quiz_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "quiz_sequence"
+    )
+    private Long id;
+    private String title;
+    private Long profileId;
+    private List<QuizQuestion> questions;
+    private Long correctAnswerIndex;
+    public Quiz() {}
+
+    public Quiz(String title, List<QuizQuestion> questions, Long profileId) {
+        this.title = title;
+        this.questions = questions;
+        this.profileId = profileId;
+    }
+
+
+}
