91874c7a25c4eb031a3473688ae3c7c5275e7420
Piotr Zapiór <piotrzapior0@gmail.com>
2024-09-28 19:15:00 +0200

    homepage

--- notec-frontend/src/App.tsx
+++ notec-frontend/src/App.tsx
@@ -6,13 +6,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route path="/login" element={<LoginPage />} />
-      </Route>,
+          <Route path="/home" element={<HomePage />} />
+      </Route>
     ),
   );
 
