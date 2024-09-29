2146383de17f728344c80512c5074f3f6663ed58
Jakub Szymczak <szymczak.kubus@gmail.com>
2024-09-29 08:46:06 +0200

    Change /api to /api/v1 controller mapping

--- demo/src/main/java/com/example/demo/quiz/QuizController.java
+++ demo/src/main/java/com/example/demo/quiz/QuizController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/quiz")
+@RequestMapping("api/v1/quiz")
 public class QuizController {
 
     private final QuizService quizService;
