591b5320c2d856c820cfed6c83d7bfd726dfaf76
Piotr Zapiór <piotrzapior0@gmail.com>
2024-09-29 03:04:52 +0200

    dbjson update

--- notec-frontend/src/pages/QuizPage.tsx
+++ notec-frontend/src/pages/QuizPage.tsx
@@ -5,17 +5,17 @@ import QuestionCard from "../components/QuestionCard.tsx";
 import {useEffect, useState} from "react";
 
 function QuizPage() {
-    const [quiz, setQuiz] = useState(null);
+    const [quiz, setQuiz] = useState(null);  // Quiz to dane quizu
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchQuiz = async () => {
             try {
-                const response = await axiosInstance.get("/quiz");
-                console.log(response.data);
-                setQuiz(response.data);
-                setLoading(false);
+                const response = await axiosInstance.get("/questions");
+                console.log('Dane z API:', response.data);  // Logowanie danych przychodzących z API
+                setQuiz(response.data);  // Ustaw dane w stanie quiz
+                setLoading(false);       // Ustaw loading na false, gdy dane są załadowane
             } catch (error) {
                 setError(error);
                 setLoading(false);
@@ -32,10 +32,7 @@ function QuizPage() {
     if (error) {
         return <div>Error: {error.message}</div>;
     }
-
-    if (!quiz) {
-        return <div>No quiz available.</div>;
-    }
+    
 
     return (
         <div className="flex flex-col h-screen">
@@ -45,10 +42,11 @@ function QuizPage() {
 
             <div className="flex-grow overflow-y-auto">
                 <div className="flex flex-col items-center space-y-4 p-4">
+                    {/* Wyświetlamy tytuł quizu */}
                     <h2>{quiz.title}</h2>
-                    <p>Tags: {quiz.tags.join(', ')}</p>
 
-                    {quiz.questions.map((question, index) => (
+                    {/* Iteracja po pytaniach */}
+                    {quiz.map((question, index) => (
                         <QuestionCard
                             key={index}
                             title={question.title}
